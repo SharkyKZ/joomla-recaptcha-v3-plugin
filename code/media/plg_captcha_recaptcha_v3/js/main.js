2cf5b606f0b0fd3ced6706c09c89b08c70b6d07a
SharkyKZ <sharkykz@gmail.com>
2024-11-29 22:11:58 +0200

    Correct license version

--- code/media/plg_captcha_recaptcha_v3/js/main.js
+++ code/media/plg_captcha_recaptcha_v3/js/main.js
@@ -1,6 +1,6 @@
 /**
  * @copyright   (C) 2023 SharkyKZ
- * @license     GPL-2.0-or-later
+ * @license     GPL-3.0-or-later
  */
 Array.from(document.querySelectorAll("input.plg-captcha-recaptcha-v3-hidden")).map(function (element) {
 	element.form.addEventListener("submit", function (event) {
